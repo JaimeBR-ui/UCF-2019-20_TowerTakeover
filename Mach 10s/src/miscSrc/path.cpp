0735e08314f1a2b732f6864c91d2abcf46329c7f
Jaime <bambas@knights.ucf.edu>
2019-07-21 19:24:31 -0400

    first edit from linux

--- Mach 10s/src/miscSrc/path.cpp
+++ Mach 10s/src/miscSrc/path.cpp
@@ -15,7 +15,7 @@ namespace path{
   );
 
   auto profileController = AsyncControllerFactory::motionProfile(
-    // sets vel, accel, and jerk//
+    // sets vel, accel, and jerk
     //max chassis velocity: 1.064 m/s
     1.0,  // Maximum linear velocity of the Chassis in m/s  1.0
     2.0,  // Maximum linear acceleration of the Chassis in m/s/s  2.0
