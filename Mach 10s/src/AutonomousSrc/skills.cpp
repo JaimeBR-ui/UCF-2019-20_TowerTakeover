1c2f1f41c391d14f7c69c40b1b09990f57fc0f95
JaimeBR-ui <bambas@knights.ucf.edu>
2019-07-31 13:53:51 -0400

    small notes added

--- Mach 10s/src/AutonomousSrc/skills.cpp
+++ Mach 10s/src/AutonomousSrc/skills.cpp
@@ -1,11 +1,12 @@
 #include "main.h"
 
-using namespace path;
+//raw pointer used since there wont be a memory leak
+//I just wanted to use a pointer for sake of consistency with naming
+//there is no need to use one and usually better to avoid them
 
 void skills(void) {
   path::makePaths();
-  //raw pointer used since there wont be a memory leak
-  AsyncMotionProfileController *chassis = &profileController;
+  AsyncMotionProfileController *chassis = &path::profileController;
   chassis->setTarget("Turn1");
   chassis->waitUntilSettled();
   pros::delay(500);
