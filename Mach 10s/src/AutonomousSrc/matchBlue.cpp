48aadf1fdd4c84274a57d87a165c7c25cdfea897
JaimeBR-ui <bambas@knights.ucf.edu>
2020-01-17 20:53:06 -0500

    removed contents

--- Mach 10s/src/AutonomousSrc/matchBlue.cpp
+++ Mach 10s/src/AutonomousSrc/matchBlue.cpp
@@ -12,46 +12,5 @@
 
 void blue(void)
 {
-/*
-  intake::move_to(SCORE, 127, true);
-  pros::delay(2000);
-  intake::move_to(RELEASE, 127, true);
-  pros::delay(2000);
-  intake::move_to(STACK, 127, true);
-  pros::delay(2000);
-  /*pros::delay(5000);
-  chassis::path::make(
-    {
-      chassis::path::point::startingPoint,
-      Point{15_in, 15_in, 90_deg}
-    },
-    "Test"
-  );
-  chassis::path::set("Test");
-  chassis::path::waitUntilSettled();*/
-  //test code
-
-  /*
-  pros::Task deploy(deployClaw);
-  chassis::path::set("Turn1");
-  chassis::path::make(
-    {
-      Point{0_ft, 0_ft, 0_deg},
-      Point{2_ft, -2.3_ft, -90_deg}
-    },
-    "Turn2"
-  );
-  chassis::path::waitUntilSettled();
-  printf("profiler ended action\n");
-  pros::delay(2000);
-  chassis::turn(900, 127, 400);
-  printf("  turn has ended\n");
-  pros::delay(500);
-  chassis::path::set("Turn2", false);
-  chassis::path::waitUntilSettled();
-  pros::delay(500);
-  chassis::turn(900, 127, 400);
-  pros::delay(500);
-  chassis::forward(48, true);
-  */
+     
 }
