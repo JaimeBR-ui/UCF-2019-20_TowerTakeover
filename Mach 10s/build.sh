2f821b48ef19301c0cd69e4f067cc7985aa65b6a
JaimeBR-ui <bambas@knights.ucf.edu>
2020-01-20 17:00:44 -0500

    (Using Pipe) Bash script that builds and uploads the program wirelessly.

---
+++ Mach 10s/build.sh
@@ -0,0 +1,13 @@
+#!bin/bash
+
+# Jaime Bohorquez
+
+# Wireless upload build script
+
+prosv5 make
+echo "y" | prosv5 upload
+
+# pros CLI commands:
+# prosv5 make
+# prosv5 upload
+# prosv5 terminal
